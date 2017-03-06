fae799a6fb7a1f3ee75b1c4e9c308b6402120568
lwy <linweiyun22@outlook.com>
2017-03-06 15:55:10 +0800

    Initial commit

---
+++ routes/users.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/:name', function(req, res, next) {
+  //res.send('respond with a resource');
+    res.send('hello, ' + req.params.name);
+});
+
+module.exports = router;
